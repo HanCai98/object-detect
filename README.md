328deb88de99c1659f11a83457a86a712e6fd215
Han <han_cai1@brown.edu>
2021-12-07 00:08:06 -0500

    update

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 Object detection model
 
 
-
-* **Download the pre-trained model**
+* **Install python and tensorflow**
 
   ```shell
-  wget https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5/
+  $ pip install tensorflow==2.4.0 python==3.7.6
   ```
+
+* **Install Dependencies**
+
+  ```shell
+  $ pip install keras==2.4.3 numpy==1.19.3 pillow==7.0.0 scipy==1.4.1 h5py==2.10.0 matplotlib==3.3.2 opencv-python keras-resnet==0.2.0
+  ```
+
+* **Install the ImageAI library**
+
+  ```shell
+  $ pip install imageai --upgrade
+  ```
+
+  
--- detection.py
+++ detection.py
@@ -4,10 +4,15 @@ import os
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
+
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
+
+# detector.setModelTypeAsTinyYOLOv3()
+# detector.setModelPath("yolo-tiny.h5")
+
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "images/image.jpeg"), output_image_path=os.path.join(execution_path , "output_images/imagenew.jpeg"), minimum_percentage_probability=30)
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "images/5.jpg"), output_image_path=os.path.join(execution_path , "output_images/imagenew5.jpeg"), minimum_percentage_probability=30)
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"], eachObject["box_points"])
